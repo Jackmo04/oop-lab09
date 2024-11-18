b8f20337f2215ec70a5ade651ba6204753a15845
Matteo Grandini <matteo.grandini@studio.unibo.it>
2024-11-18 12:27:38 +0100

    finished impl SimpleGUI

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -2,12 +2,16 @@ package it.unibo.mvc;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 /**
  * A very simple program using a graphical interface.
@@ -16,19 +20,34 @@ public final class SimpleGUI {
 
     private static final int PROPORTION = 5;
     private final JFrame frame = new JFrame();
+    private final Controller controller = new Controller();
 
     /**
      * Create a new SimpleGUI
      */
     public SimpleGUI() {
-        JPanel canvas = new JPanel();
+        final JPanel canvas = new JPanel();
         canvas.setLayout(new BorderLayout());
-        JTextArea textArea = new JTextArea();
+        final JTextArea textArea = new JTextArea();
         canvas.add(textArea, BorderLayout.CENTER);
-        JButton save = new JButton("Save");
+        final JButton save = new JButton("Save");
         canvas.add(save, BorderLayout.SOUTH);
         frame.setContentPane(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        /*
+         * Handlers
+         */
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    controller.writeToFile(textArea.getText());
+                } catch (IOException e1) {
+                    JOptionPane.showMessageDialog(frame, e, "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
     }
 
     private void display() {
@@ -42,6 +61,7 @@ public final class SimpleGUI {
 
     /**
      * Launch the application
+     * 
      * @param args unused
      */
     public static void main(final String[] args) {
